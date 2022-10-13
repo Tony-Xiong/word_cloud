e6e78bc3aeae4d5a84b5f1e23cc10e303bd6cfde
Tony <290082124@qq.com>
2022-10-13 14:16:34 +0800

    update

--- world_cloud.py
+++ world_cloud.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 import jieba
 from wordcloud import WordCloud,STOPWORDS
-from scipy.misc import imread # 处理图像的函数
 import matplotlib.pyplot as plt
 
 # 读取文本文件
@@ -9,7 +8,7 @@ text = open('words.txt', 'rb').read()
 # 对文本进行分词
 cut_text = ''.join(jieba.cut(text))
 # 读取图片
-color_mask = imread('bg.jpg')
+color_mask = plt.imread('bg.jpg')
 # 生成词云
 cloud = WordCloud(font_path='msyh.ttc',# 这里是导入字体，因为我是采用英文的，所有不导入也并不影响，若是中文的或者有其他的字符需要自己选择合适的字体包
                   background_color="white",
